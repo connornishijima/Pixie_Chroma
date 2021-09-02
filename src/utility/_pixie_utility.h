8e78ab69905ff8d9528cde10665aac97adba7980
connornishijima <connornishijima@gmail.com>
2021-09-02 16:54:52 -0600

    Update _pixie_utility.h

--- src/utility/_pixie_utility.h
+++ src/utility/_pixie_utility.h
@@ -3,6 +3,8 @@
 
 // Extra utilities for PixieChroma code, such as the gamma LUT
 
+void (*anim_func)();
+
 const uint8_t gamma8[] = {
 	0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
 	0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,  1,  1,  1,
@@ -21,8 +23,6 @@ const uint8_t gamma8[] = {
 	177,180,182,184,186,189,191,193,196,198,200,203,205,208,210,213,
 	215,218,220,223,225,228,231,233,236,239,241,244,247,249,252,255
 };
-
-void (*anim_func)();
 
 char *dtoa(double d, char *buffer, int precision) { //https://forum.arduino.cc/t/ftoa/63464
 
