bd1fa9344c601c8aa9a5fc4893624a7da74135c3
connornishijima <connornishijima@gmail.com>
2021-10-19 13:22:30 -0600

    Added Travis CI

---
+++ examples/01_Basic/01_Basic.ino
@@ -0,0 +1,20 @@
+#include "PixieChroma.h"
+PixieChroma pix;
+
+#define DATA_PIN 5
+#define PIXIES_X 6
+#define PIXIES_Y 1
+
+void setup() {
+  pix.begin(DATA_PIN, PIXIES_X, PIXIES_Y);
+  pix.set_animation(ANIMATION_NULL);
+}
+
+void loop() {
+  pix.clear();
+  pix.set_cursor(0,0);
+  pix.print(millis());
+  delay(50);
+
+  yield(); // Stupid ESP needs WDT check-in often to prevent LED flickering
+}
