1a330716918ae6c5330ab44d282b92fc8060d472
Lixie Labs <connornishijima@gmail.com>
2021-10-22 12:39:40 -0600

    Update cleanup_ci.sh

--- extras/cleanup_ci.sh
+++ extras/cleanup_ci.sh
@@ -10,4 +10,15 @@ sudo rm -r examples/*/build
 ls -l
 git commit -a -m "Auto Cleanup"
 git push
+
+echo "Installing gh to open Doxygen issues..."
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install dirmngr
+sudo apt install gh
+
+gh auth login --with-token $1
+gh issue create --title "(TESTING) Does this script automatically open an issue?" --body "If it does, this baby is working."
+
 echo "Done!" 
