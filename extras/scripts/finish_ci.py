1509e53a0e474b4693197b69227d8f1adb3e6b72
connornishijima <connornishijima@gmail.com>
2021-10-27 20:44:55 -0600

    Ported finish_ci.sh to Python

---
+++ extras/scripts/finish_ci.py
@@ -0,0 +1,22 @@
+# This is just for GitHub, and is used to clean up leftover files after
+# automatic testing has completed, and generate developer reports about
+# anything left undocumented!
+
+# run: "sudo python ./extras/scripts/finish_ci.py"
+
+import os
+import sys
+
+os.system("sudo python ./extras/scripts/generate_doxygen_report.py")
+os.system("sudo python ./extras/scripts/generate_keywords_report.py")
+os.system("sudo python ./extras/scripts/generate_icons_report.py")
+os.system("sudo python ./extras/scripts/generate_overview_report.py")
+
+print("Cleaning up CI junk...")
+os.system("git add *")
+os.system("sudo rm -r *tar*")
+os.system("sudo rm -r examples/*/build")
+os.system("git commit -a -m 'Automated Cleanup'")
+os.system("git push")
+
+print("Done!" )
