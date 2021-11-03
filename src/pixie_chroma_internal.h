3193a43f8f4debf0811cc648bda61432f042bc86
Lixie Labs <connornishijima@gmail.com>
2021-11-03 11:40:39 -0600

    Update pixie_chroma_internal.h

--- src/pixie_chroma_internal.h
+++ src/pixie_chroma_internal.h
@@ -21,7 +21,9 @@ enum t_update_mode {
 /*! ############################################################################
     @brief
     This is the software decumentation for using Pixie Chroma functions on
-    Arduino! For full example usage, see [Demos](https://DEMO_LINK.HERE).
+    Arduino!
+    
+    For full example usage, see [Demos](https://DEMO_LINK.HERE).
 *///............................................................................
 class PixieChroma{
 	public:
