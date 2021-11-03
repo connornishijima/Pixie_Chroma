666b5e44aecf06d0e9a56a2736a3db32705f2d34
Lixie Labs <connornishijima@gmail.com>
2021-11-03 10:52:21 -0600

    Update pixie_chroma_internal.cpp

--- src/pixie_chroma_internal.cpp
+++ src/pixie_chroma_internal.cpp
@@ -130,88 +130,90 @@ void PixieChroma::begin( const uint8_t data_pin, uint8_t pixies_x, uint8_t pixie
 }
 
 /*! ############################################################################
-    @brief    Initializes the display buffer, populates the XY coordinate
-              table, defaults the display colors to green, loads the default
-              CRGBPalette, initializes FastLED, and sets the default power budget.
-             
-    @details  This version of begin() is special, in that it will send your
-              image data to the LEDs in 4 parallel streams to increase speed.
-            
-              Unfortunately, this requires hard-coded pins on the ESP8266 and
-              ESP32 to function *due to FastLED limitations*:
-            
-              - DATA_OUT_1:  **GPIO 12 / D6**
-              - DATA_OUT_2:  **GPIO 13 / D7**
-              - DATA_OUT_3:  **GPIO 14 / D5**
-              - DATA_OUT_4:  **GPIO 15 / D8**
-            
-              On each data line, you'll wire `pixies_per_pin` number of Pixie
-              Chromas, with the final image being seamlessly spread across these
-              four lines.
-            
-              For example, if you have 16 Pixie Chromas (in two rows of eight) and
-              have Quad Mode enabled, then each of the 4 data lines will have 4
-              Pixie Chromas like so:
-            
-              - **DATA_OUT_1** GPIO
-                  - Pixie 1
-                  - Pixie 2
-                  - Pixie 3
-                  - Pixie 4
-              - **DATA_OUT_2** GPIO
-                  - Pixie 5
-                  - Pixie 6
-                  - Pixie 7
-                  - Pixie 8
-              - **DATA_OUT_3** GPIO
-                  - Pixie 9
-                  - Pixie 10
-                  - Pixie 11
-                  - Pixie 12
-              - **DATA_OUT_4** GPIO
-                  - Pixie 13
-                  - Pixie 14
-                  - Pixie 15
-                  - Pixie 16
-            
-              The corresponding setup for this display layout would be:
-            
-                  #include "Pixie_Chroma.h"
-                  PixieChroma pix;
-
-                  #define PIXIES_PER_PIN 4
-                  #define PIXIES_X       8
-                  #define PIXIES_Y       2
-
-                  void setup() {
-                      pix.begin_quad( PIXIES_PER_PIN, PIXIES_X, PIXIES_Y );
-                  }
-                
-              Then, with the displays physically arranged in reading order ( left to right,
-              top to bottom ) you're ready to begin!
-              
-                  DATA_OUT_1 --+                       DATA_OUT_2 ----+
-                               |                                      |
-                            +--+-+   +----+   +----+   +----+  ||  +--+-+   +----+   +----+   +----+
-                            |  1 |-->|  2 |-->|  3 |-->|  4 |  ||  |  5 |-->|  6 |-->|  7 |-->|  8 | 
-                            +----+   +----+   +----+   +----+  ||  +----+   +----+   +----+   +----+ 
-                  
-                  DATA_OUT_3 --+                       DATA_OUT_4 ----+
-                               |                                      |
-                            +--+-+   +----+   +----+   +----+  ||  +--+-+   +----+   +----+   +----+
-                            |  9 |-->| 10 |-->| 11 |-->| 12 |  ||  | 13 |-->| 14 |-->| 15 |-->| 16 | 
-                            +----+   +----+   +----+   +----+  ||  +----+   +----+   +----+   +----+ 
-            
-              Using begin_quad() to enable the Quad Mode driver will
-              always send the 4 lines of data in parallel, saving on time per frame.
-            
-              begin_quad() ideally **should not be used with less than 4 Pixie Chromas**,
-              and with at least one on each line. Even if only two lines are physically used,
-              all 4 pins are still occupied by Quad Mode.
+    @brief
+    Initializes the display buffer, populates the XY coordinate table, defaults
+    the display colors to green, loads the default CRGBPalette, initializes
+    FastLED, and sets the default power budget.
     
-    @param    pixies_per_pin  Pixies per data pin
-    @param    pixies_x        Number of Pixie PCBs in the X axis of your display
-    @param    pixies_y        Number of Pixie PCBs in the Y axis of your display
+    @details
+    This version of begin() is special, in that it will send your image data to
+    the LEDs in 4 parallel streams to increase speed.
+    
+    Unfortunately, this requires hard-coded pins on the ESP8266 and ESP32 to
+    function *due to FastLED limitations*:
+    
+    - DATA_OUT_1:  **GPIO 12 / D6**
+    - DATA_OUT_2:  **GPIO 13 / D7**
+    - DATA_OUT_3:  **GPIO 14 / D5**
+    - DATA_OUT_4:  **GPIO 15 / D8**
+    
+    On each data line, you'll wire `pixies_per_pin` number of Pixie
+    Chromas, with the final image being seamlessly spread across these
+    four lines.
+    
+    For example, if you have 16 Pixie Chromas (in two rows of eight) and
+    have Quad Mode enabled, then each of the 4 data lines will have 4
+    Pixie Chromas like so:
+    
+    - **DATA_OUT_1** GPIO
+      - Pixie 1
+      - Pixie 2
+      - Pixie 3
+      - Pixie 4
+    - **DATA_OUT_2** GPIO
+      - Pixie 5
+      - Pixie 6
+      - Pixie 7
+      - Pixie 8
+    - **DATA_OUT_3** GPIO
+      - Pixie 9
+      - Pixie 10
+      - Pixie 11
+      - Pixie 12
+    - **DATA_OUT_4** GPIO
+      - Pixie 13
+      - Pixie 14
+      - Pixie 15
+      - Pixie 16
+    
+    The corresponding setup for this display layout would be:
+    
+        #include "Pixie_Chroma.h"
+        PixieChroma pix;
+        
+        #define PIXIES_PER_PIN 4
+        #define PIXIES_X       8
+        #define PIXIES_Y       2
+        
+        void setup() {
+            pix.begin_quad( PIXIES_PER_PIN, PIXIES_X, PIXIES_Y );
+        }
+    
+    Then, with the displays physically arranged in reading order ( left to right,
+    top to bottom ) you're ready to begin!
+
+        DATA_OUT_1 --+                       DATA_OUT_2 ----+
+                     |                                      |
+                  +--+-+   +----+   +----+   +----+  ||  +--+-+   +----+   +----+   +----+
+                  |  1 |-->|  2 |-->|  3 |-->|  4 |  ||  |  5 |-->|  6 |-->|  7 |-->|  8 | 
+                  +----+   +----+   +----+   +----+  ||  +----+   +----+   +----+   +----+ 
+
+        DATA_OUT_3 --+                       DATA_OUT_4 ----+
+                     |                                      |
+                  +--+-+   +----+   +----+   +----+  ||  +--+-+   +----+   +----+   +----+
+                  |  9 |-->| 10 |-->| 11 |-->| 12 |  ||  | 13 |-->| 14 |-->| 15 |-->| 16 | 
+                  +----+   +----+   +----+   +----+  ||  +----+   +----+   +----+   +----+ 
+    
+    Using begin_quad() to enable the Quad Mode driver will
+    always send the 4 lines of data in parallel, saving on time per frame.
+
+    begin_quad() ideally **should not be used with less than 4 Pixie Chromas**,
+    and with at least one on each line. Even if only two lines are physically
+    used, all 4 pins are still occupied by Quad Mode.
+    
+    @param  pixies_per_pin  Pixies per data pin
+    @param  pixies_x        Number of Pixie PCBs in the X axis of your display
+    @param  pixies_y        Number of Pixie PCBs in the Y axis of your display
 *///............................................................................
 void PixieChroma::begin_quad( uint8_t pixies_per_pin, uint8_t pixies_x, uint8_t pixies_y ){    
     chars_x = pixies_x * 2; // Pixies have two chars each
