64b31950dc1b860ec7bde6eb363f0fcd6ea6f64f
Lixie Labs <connornishijima@gmail.com>
2021-11-03 11:26:01 -0600

    Update pixie_chroma_internal.cpp

--- src/pixie_chroma_internal.cpp
+++ src/pixie_chroma_internal.cpp
@@ -368,7 +368,7 @@ void PixieChroma::set_gamma_correction( bool enabled ){
 /*! ############################################################################
     @brief
     Sets the cursor position in a 2D context, in whole displays.
-            
+    
     @details  
     Wherever the cursor is, is where the next call to print() or write() will
     originate. Position is counted from zero. Remember: each Pixie Chroma has
