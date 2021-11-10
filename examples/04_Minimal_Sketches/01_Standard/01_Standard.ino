9e1262f5b4db90ec3fffc0b43a5fece01c972d6e
connornishijima <connornishijima@gmail.com>
2021-11-10 16:23:44 -0700

    Added template example sketches

---
+++ examples/04_Minimal_Sketches/01_Standard/01_Standard.ino
@@ -0,0 +1,20 @@
+// This is a bare-minimum Pixie Chroma sketch using single-line mode!
+// Use this template to easily build your own Pixie Chroma sketches.
+
+#include "Pixie_Chroma.h" // ... Include library
+PixieChroma pix; // ............ Get class object
+
+#define DATA_PIN  12 // GPIO to use for Pixie Chroma data line
+#define PIXIES_X  2  // Total amount and arrangement
+#define PIXIES_Y  1  // of Pixie PCBs
+
+void setup() {
+  pix.begin( DATA_PIN, PIXIES_X, PIXIES_Y );
+  pix.color( CRGB::Cyan );
+}
+
+void loop() {
+  pix.clear();
+  pix.print( millis() );
+  pix.show();
+}
---
+++ examples/04_Minimal_Sketches/02_Quad_Mode/02_Quad_Mode.ino
@@ -0,0 +1,29 @@
+// This is a bare-minimum Pixie Chroma sketch using Quad-line mode!
+// Use this template to easily build your own Pixie Chroma sketches.
+
+#include "Pixie_Chroma.h" // ... Include library
+PixieChroma pix; // ............ Get class object
+
+#define PIXIES_PER_PIN  3 // Number of Pixie PCBs per GPIO
+#define PIXIES_X        6 // Total amount and arrangement
+#define PIXIES_Y        2 // of Pixie PCBs
+                          //
+						  // 4 lines with 3 Pixies == 12 total
+
+// Quad Mode GPIO, same on ESP8266 and ESP32:
+
+// DATA_OUT_1: GPIO 12 / D6
+// DATA_OUT_2: GPIO 13 / D7
+// DATA_OUT_3: GPIO 14 / D5
+// DATA_OUT_4: GPIO 15 / D8
+
+void setup() {
+  pix.begin_quad( PIXIES_PER_PIN, PIXIES_X, PIXIES_Y );
+  pix.color( CRGB::Cyan );
+}
+
+void loop() {
+  pix.clear();
+  pix.print( millis() );
+  pix.show();
+}
