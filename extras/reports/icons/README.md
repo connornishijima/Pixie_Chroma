4de2346ee12f9e6c91524956227ce3e1b23de97a
connornishijima <connornishijima@gmail.com>
2021-11-13 02:15:21 -0700

    Removed Icon reports

--- extras/reports/icons/README.md
+++
@@ -1,4 +0,0 @@
-### Icon coverage report: 
-#### Any undocumented Icons currently seen will appear here after every CI test!
----------------------------------------------------------
-:heavy_check_mark: **All checks passed, nothing left undocumented!**
--- extras/scripts/gamma.py
+++ extras/scripts/gamma.py
@@ -2,19 +2,19 @@
 import math
 
 def gamma(nsteps, gamma):
-    gammaedUp = [math.pow(x, gamma) for x in range(nsteps)]
-    return [x/max(gammaedUp) for x in gammaedUp]
+    gammaed_up = [math.pow(x, gamma) for x in range(nsteps)]
+    return [x/max(gammaed_up) for x in gammaed_up]
 
-def rounder(topValue, gammas):
-    return [min(topValue, round(x*topValue)) for x in gammas]
+def rounder(top_value, gammas):
+    return [min(top_value, round(x*top_value)) for x in gammas]
 
 if __name__ == "__main__":
-    myGamma = 2.314
+    in_gamma = 2.314
     steps = 256
     out_string = ""
-    out_string += ("/* %d-step brightness table: gamma = %s */ \n\n" % (steps, myGamma))
+    out_string += ("/* %d-step brightness table: gamma = %s */ \n\n" % (steps, in_gamma))
     out_string += ("const uint8_t gamma_table[%d] = {\n" % steps)
-    for value in rounder(255, gamma(steps, myGamma)):
+    for value in rounder(255, gamma(steps, in_gamma)):
         out_string += ("\t %d,\n" % value)
     out_string += ("};\n")
     
