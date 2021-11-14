e777035349b7d691d9825f2a574bbaecc9e3b9fe
connornishijima <connornishijima@gmail.com>
2021-11-13 18:26:21 -0700

    Cleaned empty examples for faster CI

--- examples/02_Advanced/08_Power_Budgeting/08_Power_Budgeting.ino
+++ examples/02_Advanced/08_Power_Budgeting/08_Power_Budgeting.ino
@@ -1,18 +1,7 @@
 // TODO: 08_Power_Budgeting.ino needs content
 
-#include "Pixie_Chroma.h"
-PixieChroma pix;
-
-#define DATA_PIN	3
-#define PIXIES_X	6
-#define PIXIES_Y	2
-
 void setup() {
-  Serial.begin(250000);
-  pix.begin(DATA_PIN, PIXIES_X, PIXIES_Y);
-  delay(200);
 }
 
 void loop() {
-  pix.clear();
 }
