88ac3bcddbb93226cac8a90eb489171578507fdf
connornishijima <connornishijima@gmail.com>
2021-12-03 01:53:31 -0700

    Moved docs directory to extras

--- docs/search/all_d.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['t_5fjustification_87',['t_justification',['../pixie__chroma__internal_8h.html#a3ab09b0eefc7f5e256af77389b23874a',1,'pixie_chroma_internal.h']]],
-  ['t_5fupdate_5fmode_88',['t_update_mode',['../pixie__chroma__internal_8h.html#a8dbce326e7d153234a6fa2f171dfd19c',1,'pixie_chroma_internal.h']]]
-];
--- docs/search/classes_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['pixiechroma_97',['PixieChroma',['../class_pixie_chroma.html',1,'']]]
-];
