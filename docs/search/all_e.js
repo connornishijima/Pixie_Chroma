c61d6bcae38e8500e6ee8310b085e7d69e269fc2
connornishijima <connornishijima@gmail.com>
2021-12-03 02:07:59 -0700

    .

--- docs/search/all_e.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['unit_5ftest_5fpixies_5fper_5fpin_89',['UNIT_TEST_PIXIES_PER_PIN',['../_pixie___chroma_8h.html#a2d5fb091a92d930934626f7e84794ea6',1,'Pixie_Chroma.h']]],
-  ['unit_5ftest_5fpixies_5fx_90',['UNIT_TEST_PIXIES_X',['../_pixie___chroma_8h.html#a2127e2c4498dd288097c0c087f53facd',1,'Pixie_Chroma.h']]],
-  ['unit_5ftest_5fpixies_5fy_91',['UNIT_TEST_PIXIES_Y',['../_pixie___chroma_8h.html#abefc2b4760880b3f0be7e0947b1e7a2c',1,'Pixie_Chroma.h']]],
-  ['unit_5ftests_92',['unit_tests',['../class_pixie_chroma.html#aa2c9f1cf733e611015b7b680961c561f',1,'PixieChroma']]],
-  ['uv_93',['uv',['../class_pixie_chroma.html#a8469c787620cde68a0b9d0678744f4c2',1,'PixieChroma']]]
-];
--- docs/search/enumvalues_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['automatic_189',['AUTOMATIC',['../pixie__chroma__internal_8h.html#a8dbce326e7d153234a6fa2f171dfd19ca0a831c2bc18e8354fe3e30ec0f3cdcda',1,'pixie_chroma_internal.h']]]
-];
